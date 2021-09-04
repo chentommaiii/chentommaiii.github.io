03a6d4e5e663d2b9ddf00526ca97ffe1d9fb8aa4
chentommaiii <85600799+chentommaiii@users.noreply.github.com>
2021-09-04 20:29:51 +0800

    side navigation icon

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Get the navbar
 var header = document.querySelector("header");
 
+var logo = document.getElementById("logo");
+
 var sideNav = document.getElementById("mySideNav");
 var main = document.getElementById("main");
 var header = document.getElementById("header");
@@ -8,6 +10,8 @@ var header = document.getElementById("header");
 var sideNavIcon = document.getElementsByClassName("sidenav-icon")[0];
 
 const container = document.getElementsByClassName("container")[0];
+
+let sideNavIsOpen = false;
 
 window.addEventListener("scroll", parralax)
 
@@ -19,22 +23,36 @@ function myFunction() {
   header.classList.toggle("sticky", window.scrollY > 0);
 }
 
-function openNav() {
-  sideNav.style.width = "300px";
-  main.style.left="300px";
-  header.style.marginLeft = "300px";
+logo.onclick = function() {
+
+  if (sideNavIsOpen == false) {
+
+    sideNav.style.width = "300px";
+    main.style.left="300px";
+    header.style.marginLeft = "300px";
+
+    sideNavIcon.style.transform = "rotate(135deg)";
+    sideNavIcon.style.webkitTransform = "rotate(135deg)";
+
+    sideNavIsOpen = true;
+
+  } else {
+
+    /* Set the width of the side navigation to 0 and the left margin of the page content to 0 to hide side nav*/
+    sideNav.style.width = "0";
+    main.style.left="0";
+    header.style.marginLeft = "0";
+
+    sideNavIcon.style.transform = "rotate(-45deg)";
+    sideNavIcon.style.webkitTransform = "rotate(-45deg)";
+
+    sideNavIsOpen = false;
+
+  }  
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-function closeNav() {
-  sideNav.style.width = "0";
-  main.style.left="0";
-  header.style.marginLeft = "0";
-}
 
 function parralax() {
   let offset = window.pageYOffset;
   container.style.backgroundPositionY = offset * 0.8 + "px";
 }
-
-document.getElementById("logo").addEventListener("click", openNav);
